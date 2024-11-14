94cc94fc89b7c456ec3240b4ecdca5776f813b27
Alex Tavendale <9274678+atavendale@users.noreply.github.com>
2024-11-14 19:27:20 +0000

    set cirrus completion suggester

--- config/mediawiki/LocalSettings.php
+++ config/mediawiki/LocalSettings.php
@@ -191,6 +191,7 @@ $wgDiff3 = "/usr/bin/diff3";
 // $wgDisableSearchUpdate = true;
 $wgCirrusSearchServers = [ getenv( 'ES_IP' ) ];
 $wgSearchType = 'CirrusSearch';
+$wgCirrusSearchUseCompletionSuggester = 'build';
 
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, e.g. 'vector' or 'monobook':
