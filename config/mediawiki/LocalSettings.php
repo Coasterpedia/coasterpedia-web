cf76b44dee4e9172477431df0e9e14f701c6a9f8
Alex Tavendale <9274678+atavendale@users.noreply.github.com>
2025-01-13 22:19:15 +0000

    show exception details

--- config/mediawiki/LocalSettings.php
+++ config/mediawiki/LocalSettings.php
@@ -9,7 +9,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 $wgSitename = "Coasterpedia";
 $wgMetaNamespace = "Coasterpedia";
 
-// $wgShowExceptionDetails = true;
+$wgShowExceptionDetails = true;
 
 ## The URL base path to the directory containing the wiki;
 ## defaults for all runtime URL paths are based off of this.
