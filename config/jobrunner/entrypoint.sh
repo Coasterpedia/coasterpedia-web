95a0e35f072cae3ffbfb2b85436751b10e243e37
Alex Tavendale <9274678+atavendale@users.noreply.github.com>
2025-06-26 22:43:43 +0100

    fix

--- config/jobrunner/entrypoint.sh
+++ config/jobrunner/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "${RUNNER_TYPE:-job}" = "Chron" ]; then
-   /usr/local/bin/php /var/www/html/w/mediawiki-services-jobrunner/redisJobChronService --config-file=/var/www/html/mediawiki-services-jobrunner/jobrunner-conf.json --verbose
+   /usr/local/bin/php /var/www/html/w/mediawiki-services-jobrunner/redisJobChronService --config-file=/var/www/html/w/mediawiki-services-jobrunner/jobrunner-conf.json --verbose
 else
-   /usr/local/bin/php /var/www/html/w/mediawiki-services-jobrunner/redisJobRunnerService --config-file=/var/www/html/mediawiki-services-jobrunner/jobrunner-conf.json --verbose
+   /usr/local/bin/php /var/www/html/w/mediawiki-services-jobrunner/redisJobRunnerService --config-file=/var/www/html/w/mediawiki-services-jobrunner/jobrunner-conf.json --verbose
 fi
