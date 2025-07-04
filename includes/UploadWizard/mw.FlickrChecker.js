9c9565ade5b2df984377a97450da5380018f31ee
Alex Tavendale <9274678+atavendale@users.noreply.github.com>
2025-07-04 21:08:42 +0100

    update flickr licenses

--- includes/UploadWizard/mw.FlickrChecker.js
+++ includes/UploadWizard/mw.FlickrChecker.js
@@ -51,16 +51,29 @@ mw.FlickrChecker.licenseList = [];
 // Live list at http://api.flickr.com/services/rest/?&method=flickr.photos.licenses.getInfo&api_key=...
 mw.FlickrChecker.licenseMaps = {
 	'All Rights Reserved': 'invalid',
+	'CC BY 2.0': '{{cc-by-2.0}}{{flickrreview}}',
+	'CC BY-ND 2.0': '{{cc-by-nd-2.0}}{{flickrreview}}',
+	'CC BY-NC-ND 2.0': '{{cc-by-nc-nd-2.0}}{{flickrreview}}',
+	'CC BY-NC 2.0': '{{cc-by-nc-2.0}}{{flickrreview}}',
+	'CC BY-NC-SA 2.0': '{{cc-by-nc-sa-2.0}}{{flickrreview}}',
+	'CC BY-SA 2.0': '{{cc-by-sa-2.0}}{{flickrreview}}',
+	'No known copyright restrictions': '{{Flickr-no known copyright restrictions}}{{flickrreview}}',
+	'United States Government Work': '{{PD-USGov}}{{flickrreview}}',
+	'Public Domain Dedication (CC0)': '{{cc-zero}}{{flickrreview}}',
+	'Public Domain Mark': '{{PD-US}}{{flickrreview}}',
+	'CC BY 4.0': '{{cc-by-4.0}}{{flickrreview}}',
+	'CC BY-ND 4.0': '{{cc-by-nd-4.0}}{{flickrreview}}',
+	'CC BY-NC-ND 4.0': '{{cc-by-nc-nd-4.0}}{{flickrreview}}',
+	'CC BY-NC 4.0': '{{cc-by-nc-4.0}}{{flickrreview}}',
+	'CC BY-NC-SA 4.0': '{{cc-by-nc-sa-4.0}}{{flickrreview}}',
+	'CC BY-SA 4.0': '{{cc-by-sa-4.0}}{{flickrreview}}',
+	// the following are old flickr license names from 2011, preserved here just in case
 	'Attribution License': '{{cc-by-2.0}}{{flickrreview}}',
-        'Attribution-NoDerivs License': '{{cc-by-nd-2.0}}{{flickrreview}}',
-        'Attribution-NonCommercial-NoDerivs License': '{{cc-by-nc-nd-2.0}}{{flickrreview}}',
-        'Attribution-NonCommercial License': '{{cc-by-nc-2.0}}{{flickrreview}}',
-        'Attribution-NonCommercial-ShareAlike License': '{{cc-by-nc-sa-2.0}}{{flickrreview}}',
-        'Attribution-ShareAlike License': '{{cc-by-sa-2.0}}{{flickrreview}}',
-        'No known copyright restrictions': '{{Flickr-no known copyright restrictions}}{{flickrreview}}',
-        'United States Government Work': '{{PD-USGov}}{{flickrreview}}',
-        'Public Domain Dedication (CC0)': '{{cc-zero}}{{flickrreview}}',
-        'Public Domain Mark': '{{flickrreview}}' // T105629, user needs to add a valid PD licens
+	'Attribution-NoDerivs License': '{{cc-by-nd-2.0}}{{flickrreview}}',
+	'Attribution-NonCommercial-NoDerivs License': '{{cc-by-nc-nd-2.0}}{{flickrreview}}',
+	'Attribution-NonCommercial License': '{{cc-by-nc-nd-2.0}}{{flickrreview}}',
+	'Attribution-NonCommercial-ShareAlike License': '{{cc-by-nc-sa-2.0}}{{flickrreview}}',
+	'Attribution-ShareAlike License': '{{cc-by-sa-2.0}}{{flickrreview}}'
 };
 
 mw.FlickrChecker.prototype = {
